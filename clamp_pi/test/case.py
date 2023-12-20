4fa89ddc11b3c007a92e66107d09c99c2ac20702
Kiran Sreekumar <52482677+EvilChangIndia@users.noreply.github.com>
2023-12-20 12:21:07 +0100

    V3

--- clamp_pi/test/case.py
+++
@@ -1,9 +0,0 @@
-i=int(input("enter value: "))
-match i:
-	case 1:
-		print("one")
-	case 2:
-		print("two")
-	case _ :
-		print("i've not been taught that yet")
-
--- clamp_pi/test/terminalTest.py
+++
@@ -1,9 +0,0 @@
-
-
-from subprocess import call
-call(["sudo" "ip" "link" "set" "can0" "down"])
-time.sleep(0.5)
-call(["sudo", "ip", "link", "set", "can0", "up", "type", "can", "bitrate", "125000"])
-time.sleep(1)
-call(["sudo", "ifconfig", "can0", "txqueuelen", "10000"])
-time.sleep(0.5)
